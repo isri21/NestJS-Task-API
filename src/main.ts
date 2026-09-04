56a05271e52ad0f151fcd6282526554dfdb6c9c1
izzy <israelalemu21@gmail.com>
2026-09-04 15:48:15 +0300

    fix: configure CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors({
+    origin: ["http://localhost:3001"]
+  })  
 
   const config = new DocumentBuilder()
     .setTitle('Simple Task API')
