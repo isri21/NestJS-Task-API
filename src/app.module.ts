163c7d2ca2bfea394a4efd55cd8a23380f47c84f
izzy <israelalemu21@gmail.com>
2026-09-07 10:09:08 +0300

    refactor: configure application to use environment variables instead of hardcoded values

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,28 +4,39 @@ import { TaskModule } from './task/task.module.js';
 import { CacheModule } from '@nestjs/cache-manager';
 import { CachesModule } from './caches/caches.module.js';
 import { createKeyv, Keyv } from '@keyv/redis';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'root',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true,
+  imports: [
+    ConfigModule.forRoot({
+     isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+      type: 'postgres',
+      host: config.get('DB_HOST'),
+      port: config.get('DB_PORT'),
+      username: config.get('DB_USERNAME'),
+      password: config.get('DB_PASSWORD'),
+      database: config.get('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+    })
   }),
-  TaskModule,
   CacheModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
     isGlobal: true,
-    useFactory: async () => {
-      const keyv: Keyv = createKeyv('redis://localhost:6379')
+    useFactory: async (config: ConfigService) => {
+      const keyv: Keyv = createKeyv(`redis://${config.get('REDIS_HOST')}:${config.get('REDIS_PORT')}`)
       keyv.on('error', (err) => console.error('KEYV/REDIS ERROR:', err))
       return { stores: [keyv] }
     }
   }),
-  CachesModule,
+  TaskModule,
+  CachesModule
   ],
   controllers: [],
   providers: [],
