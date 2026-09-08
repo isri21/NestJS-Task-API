2a4a16c49668493bd200cdf098792c2346fd5a28
izzy <israelalemu21@gmail.com>
2026-09-08 11:28:30 +0300

    fix lint and test errors

--- src/caches/caches.controller.ts
+++ src/caches/caches.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { CachesService } from './caches.service.js';
-import { UpdateCachDto } from './dto/update-cach.dto.js';
 import { CreateCachDto } from './dto/create-cach.dto.js';
 
 @Controller('caches')
